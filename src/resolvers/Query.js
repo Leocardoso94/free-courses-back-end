4cf21dab2381a6561357ddc4db15b26c742d7c24
Leonardo Cardoso <leocardosoti@gmail.com>
2018-06-13 23:26:11 -0300

    fazendo as consultas de categorias

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,39 +1,18 @@
 import {
   getGSheetsValueCategories,
-  getGSheetsValueCourses,
-  normalizeSheetsData,
 } from '../utils';
 
-// async function feed(parent, args, ctx, info) {
-//   const { filter, first, skip } = args // destructure input arguments
-//   const where = filter
-//     ? { OR: [{ url_contains: filter }, { description_contains: filter }] }
-//     : {}
 
-//   const allLinks = await ctx.db.query.links({})
-//   const count = allLinks.length
-
-//   const queriedLinkes = await ctx.db.query.links({ first, skip, where })
-
-//   return {
-//     linkIds: queriedLinkes.map(link => link.id),
-//     count
-//   }
-// }
-
-export const info = async () => {
-  const sheetsData = await getGSheetsValueCourses();
-
-  const courses = normalizeSheetsData(sheetsData);
-
-  console.log(courses[1]);
-
-  return '';
-};
+export const categories = (parent, {
+  limit = 10,
+  offset = 0,
+}) => getGSheetsValueCategories(limit, offset);
 
 
-export const categories = async () => {
-  const data = await getGSheetsValueCategories();
+export const findCategoryById = async (parent, {
+  id,
+}) => {
+  const categoriesData = await getGSheetsValueCategories();
 
-  return '';
+  return categoriesData.find(category => category.id === id);
 };
--- src/utils.js
+++ src/utils.js
@@ -3,6 +3,9 @@ import {
   API_URL,
   API_KEY,
 } from '../src/constants';
+import {
+  isNumber,
+} from 'util';
 
 const request = axios.create({
   baseURL: API_URL,
@@ -11,19 +14,26 @@ const request = axios.create({
   },
 });
 
+export const normalizeSheetsData = (sheetsData = [], keys = []) => sheetsData
+  .map(values => values
+    .map((value, index) => ({
+      [keys[index]]: value,
+    }))
+    .reduce((prev, obj) => Object.assign(prev, obj), {}));
+
 export const getGSheetsValue = async params => (await request.get(`/values/${params}`)).data;
 
-export const getGSheetsValueCategories = async () => (await getGSheetsValue('categories!A1:Z')).values;
+export const getGSheetsValueCategories = async (limit, offset) => {
+  const sheetsData = (await getGSheetsValue('categories!A1:Z')).values;
+
+  const keys = sheetsData.shift();
+
+  if (isNumber(limit) &&
+    isNumber(offset)) return normalizeSheetsData(sheetsData.slice(offset, limit), keys);
+
+  return normalizeSheetsData(sheetsData, keys);
+};
 
 export const getGSheetsValueCourses = async () => (await getGSheetsValue('courses!A1:Z')).values;
 
 export const getKeys = arr => arr[0];
-
-export const normalizeSheetsData = (sheetsData = []) => {
-  const keys = sheetsData.shift();
-
-  return sheetsData
-    .map(values => values.map((value, index) => ({
-      [keys[index]]: value,
-    })));
-};
